13c5dd3e315dbddb5558e8b9271c68c921833183
nourhanaa-mohye <125581092+nourhanaa-mohye@users.noreply.github.com>
2023-09-20 09:30:34 -0400

    task3

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const admin = require('admin');
+
+const app = express();
+const port = 3000;
+const serviceAccount = require("C:\\Users\\Documents\\Tasks\\Firebase_Task\\test-a3669-firebase-adminsdk-xa0h0-2eb82997a3.json"); 
+app.use(express.json()) 
+
+admin.initializeApp({ 
+    credential: admin.credential.cert(serviceAccount) }); 
+ 
+app.post('/', (req, res) => { 
+    const data = admin.firestore(); 
+    const Ref = db.collection("test").doc("demo"); 
+    collectionRef.set(req.body) 
+}) 
+ 
+app.listen(3000, () => { console.log(`Server running on http://${serverIp}:${port}`);
